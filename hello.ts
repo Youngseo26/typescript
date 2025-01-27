f63d517c6aa35c7c3ad5245066d5c7bc6177655e
김영서 <dudtj926@naver.com>
2025-01-27 21:37:34 +0900

    타입스크립트 설치 및 기본타입
    타입스크립트 설치 및 기본타입

---
+++ hello.ts
@@ -0,0 +1,36 @@
+console.log("Hello Typescript");
+
+let age: number;
+age = 29;
+
+let userName: string;
+userName = "Youngseo";
+
+let hobbies: string[];
+hobbies = ["Cooking", "Climbing"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Youngseo",
+  age: 29,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+people = [
+  {
+    name: "Youngseo",
+    age: 29,
+  },
+  {
+    name: "Myounggon",
+    age: 29,
+  },
+];
